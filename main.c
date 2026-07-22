8e2fe8f201e1a3f33da2da2b76edcc788749e6b6
gm1224 <206447435+gm1224@users.noreply.github.com>
2026-07-22 18:10:08 -0400

    Cleanup

--- main.c
+++ main.c
@@ -9,6 +9,14 @@
 #define SCALE 30
 
 typedef struct {
+	uint16_t NNN;
+	uint8_t N;
+	uint8_t X;
+	uint8_t Y;
+	uint8_t KK;
+} instruction_t;
+
+typedef struct {
 	uint8_t memory[4096];
 	uint8_t V[16];
 	uint16_t I;
@@ -19,6 +27,7 @@ typedef struct {
 	bool display[64 * 32];
 	uint16_t opcode;
 	bool keypad[16];
+	instruction_t instruction;
 } chip8_t;
 
 bool running = true;
@@ -67,7 +76,6 @@ bool chipInit(chip8_t *chip8, const char rom[]) {
 	chip8->PC = 0x200;
 	chip8->stack_pointer = &chip8->stack[0];
 
-
 	return true;
 }
 
@@ -76,7 +84,13 @@ void execute(chip8_t *chip8) {
 	chip8->opcode = (chip8->memory[chip8->PC] << 8) | chip8->memory[chip8->PC + 1];
 	chip8->PC += 2;
 
-	printf("Using 0x%04X\n", chip8->opcode);
+	chip8->instruction.NNN = (chip8->opcode & 0x0FFF); // NNN, lowest 12 bits
+	chip8->instruction.N = (chip8->opcode & 0x000F); // N, lowest 4 bits
+	chip8->instruction.X = ((chip8->opcode >> 8) & 0x000F); // X, lower 4 bits of high byte
+	chip8->instruction.Y = ((chip8->opcode >> 4) & 0x000F); // Y, upper 4 bits of low byte
+	chip8->instruction.KK = (chip8->opcode & 0x00FF);
+
+	printf("Executing 0x%04X\n", chip8->opcode);
 
 	// TODO: Stack over/underflow checks
 	switch ((chip8->opcode >> 12) & 0x0F) {
@@ -91,105 +105,105 @@ void execute(chip8_t *chip8) {
 					break;
 				default:
 					// Should only be 0nnn.
-					printf("Instruction 0x%04X Skipped.\n", chip8->opcode);
+					printf("Instruction 0x%04X ignored.\n", chip8->opcode);
 					break;
 			}
 			break;
 		case 0x01: // 1nnn (JP addr): Jump to location nnn.
-			chip8->PC = (chip8->opcode & 0x0FFF);
+			chip8->PC = (chip8->instruction.NNN);
 			break;
 		case 0x02: // 2nnn (CALL addr): Call subroutine at nnn.
 			*chip8->stack_pointer = chip8->PC;
 			chip8->stack_pointer++;
-			chip8->PC = (chip8->opcode & 0x0FFF);
+			chip8->PC = (chip8->instruction.NNN);
 			break;
 		case 0x03: // 3xkk (SE Vx, byte): Skip next instruction if Vx = kk.
-			if (chip8->V[(chip8->opcode >> 8) & 0x0F] == (chip8->opcode & 0x00FF)) {
+			if (chip8->V[chip8->instruction.X] == chip8->instruction.KK) {
 				chip8->PC +=2;
 			}
 			break;
 		case 0x04: // 4xkk (SNE Vx, byte): Skip next instruction if Vx != kk.
-			if (chip8->V[(chip8->opcode >> 8) & 0x0F] != (chip8->opcode & 0x00FF)) {
+			if (chip8->V[chip8->instruction.X] != chip8->instruction.KK) {
 				chip8->PC += 2;
 			}
 			break;
 		case 0x05: // 5xy0 (SE Vx, Vy): Skip next instruction if Vx = Vy.
-			if (chip8->V[(chip8->opcode >> 8) & 0x0F] == chip8->V[(chip8->opcode >> 4) & 0x0F]) {
+			if (chip8->V[chip8->instruction.X] == chip8->V[chip8->instruction.Y]) {
 				chip8->PC += 2;
 			}
 			break;
 		case 0x06: // 6xkk (LD Vx, byte): Set Vx = kk.
-			chip8->V[(chip8->opcode >> 8) & 0x0F] = (chip8->opcode & 0x00FF);
+			chip8->V[chip8->instruction.X] = chip8->instruction.KK;
 			break;
 		case 0x07: // 7xkk (ADD Vx, byte): Set Vx = Vx + kk.
-			chip8->V[(chip8->opcode >> 8) & 0x0F] += (chip8->opcode & 0x00FF);
+			chip8->V[chip8->instruction.X] += chip8->instruction.KK;
 			break;
 		case 0x08:
-			switch (chip8->opcode & 0x000F) {
-				case 0x00:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] = chip8->V[(chip8->opcode >> 4) & 0x0F];
+			switch (chip8->opcode & 0x0F) {
+				case 0x00: // 8xy0 (LD Vx, Vy): Stores value of Vy in Vx.
+					chip8->V[chip8->instruction.X] = chip8->V[chip8->instruction.Y];
 					break;
-				case 0x01:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] |= chip8->V[(chip8->opcode >> 4) & 0x0F]; 
+				case 0x01: // 8xy1 (OR Vx, Vy): Perform bitwise OR on Vx and Vy and store result in Vx.
+					chip8->V[chip8->instruction.X] |= chip8->V[chip8->instruction.Y]; 
 					break;
-				case 0x02:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] &= chip8->V[(chip8->opcode >> 4) & 0x0F];
+				case 0x02: // 8xy2 (AND Vx, Vy): Perform bitwise AND on Vx and Vy and store result in Vx.
+					chip8->V[chip8->instruction.X] &= chip8->V[chip8->instruction.Y];
 					break;
-				case 0x03:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] ^= chip8->V[(chip8->opcode >> 4) & 0x0F];
+				case 0x03: // 8xy3 (XOR Vx, Vy): Perform bitwise XOR on Vx and Vy and store the result in Vx.
+					chip8->V[chip8->instruction.X] ^= chip8->V[chip8->instruction.Y];
 					break;
-				case 0x04:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] += chip8->V[(chip8->opcode >> 4) & 0x0F];
-					if (chip8->V[(chip8->opcode >> 8) & 0x0F] > 255) {
+				case 0x04: // 8xy4 (ADD Vx, Vy): Vx and Vy are added together and sets VF = 1 if the result is greater than 255 (8 bits).
+					chip8->V[chip8->instruction.X] += chip8->V[chip8->instruction.Y];
+					if (chip8->V[chip8->instruction.X] > 255) {
 						chip8->V[0xF] = 1;
 					}
 					else {
 						chip8->V[0xF] = 0;
 					}
 					break;
-				case 0x05:
-					if (chip8->V[(chip8->opcode >> 8) & 0x0F] > chip8->V[(chip8->opcode >> 4) & 0x0F]) {
+				case 0x05: // 8xy5 (SUB Vx, Vy): If Vx > Vy then VF = 1, then subtract Vy from Vx and store result in Vx.
+					if (chip8->V[chip8->instruction.X] > chip8->V[chip8->instruction.Y]) {
 						chip8->V[0xF] = 1;
 					}
 					else {
 						chip8->V[0xF] = 0;
 					}
 
-					chip8->V[(chip8->opcode >> 8) & 0x0F] = chip8->V[(chip8->opcode >> 8) & 0x0F] - chip8->V[(chip8->opcode >> 4) & 0x0F];
+					chip8->V[chip8->instruction.X] -= chip8->V[chip8->instruction.Y];
 					break;
-				case 0x06:
-					chip8->V[0xF] = chip8->V[(chip8->opcode >> 8) & 0x0F] & 0x01;
-					chip8->V[(chip8->opcode >> 8) & 0x0F] >>= 1;
+				case 0x06: // 8xy6 (SHR Vx {, Vy}): If least significant bit of Vx is 1, then VF = 1, also divide Vx by 2.
+					chip8->V[0xF] = chip8->V[chip8->instruction.X] & 0x01;
+					chip8->V[chip8->instruction.X] >>= 1;
 					break;
-				case 0x0E:
-					chip8->V[0xF] = chip8->V[(chip8->opcode >> 8) & 0x0F] & 0x01;
-					chip8->V[(chip8->opcode >> 8) & 0x0F] <<= 1;
+				case 0x0E: // 8xyE (SHL Vx {, Vy}): If most significant bit of Vx is 1, then VF = 1, also multiply Vx by 2
+					chip8->V[0xF] = chip8->V[chip8->instruction.X] & 0x01;
+					chip8->V[chip8->instruction.X] <<= 1;
 					break;
 				default:
 					printf("Invalid Instruction: 0x%04X\n", chip8->opcode);
-					//running = false;
+					running = false;
 					break;
 			}
 			break;
 		case 0x09: // 9xy0 (SNE Vx, Vy): Skip next instruction if Vx != Vy.
-			if (chip8->V[(chip8->opcode >> 8) & 0x0F] != chip8->V[(chip8->opcode >> 4) & 0x0F]) {
+			if (chip8->V[chip8->instruction.X] != chip8->V[chip8->instruction.Y]) {
 				chip8->PC += 2;
 			}
 			break;
 		case 0x0A: // Annn (LD I, addr): Set I = nnn.
-			chip8->I = chip8->opcode & 0x0FFF;
+			chip8->I = chip8->instruction.NNN;
 			break;
 		case 0x0B: // Bnnn (JP V0, addr): Jump to location nnn + V0.
-			chip8->PC = (chip8->opcode & 0x0FFF) + chip8->V[0];
+			chip8->PC = chip8->instruction.NNN + chip8->V[0];
 			break;
 		case 0x0C: // Cxkk (RND Vx, byte): Set Vx = random byte AND kk.
-			chip8->V[(chip8->opcode >> 8) & 0x0F] = ((rand() % 256) & (chip8->opcode & 0x00FF));
+			chip8->V[chip8->instruction.X] = ((rand() % 256) & (chip8->instruction.KK));
 			break;
 		case 0x0D: // Dxyn (DRW Vx, Vy, niblle): Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
 			// TODO: Just rewrite this
 			chip8->V[0xF] = 0; // Reset VF
-			uint8_t xCoord = chip8->V[(chip8->opcode >> 8) & 0x0F] % 64;
-			uint8_t yCoord = chip8->V[(chip8->opcode >> 4) & 0x0F] % 32;
+			uint8_t xCoord = chip8->V[chip8->instruction.X] % 64;
+			uint8_t yCoord = chip8->V[chip8->instruction.Y] % 32;
 
 			for (uint8_t i = 0; i < (chip8->opcode & 0x0F); i++) {
 				uint8_t byte = chip8->memory[chip8->I + i];
@@ -217,13 +231,13 @@ void execute(chip8_t *chip8) {
 			break;
 		case 0x0E:
 			switch (chip8->opcode & 0x00FF) {
-				case 0x9E:
-					if (chip8->keypad[chip8->V[(chip8->opcode >> 8) & 0x0F]] == true) {
+				case 0x9E: // 0xEx9E (SKP Vx): Skip next instruction if key with the value of Vx is pressed.
+					if (chip8->keypad[chip8->V[chip8->instruction.X]] == true) {
 						chip8->PC += 2;
 					}
 					break;
-				case 0xA1:
-					if (chip8->keypad[chip8->V[(chip8->opcode >> 8) & 0x0F]] == false) {
+				case 0xA1: // 0xExA1 (SKNP Vx): Skip next instruction if key with the value of Vx is not pressed.
+					if (chip8->keypad[chip8->V[chip8->instruction.X]] == false) {
 						chip8->PC += 2;
 					}
 					break;
@@ -235,17 +249,17 @@ void execute(chip8_t *chip8) {
 			break;
 		case 0x0F:
 			switch (chip8->opcode & 0x00FF) {
-				case 0x07:
-					chip8->V[(chip8->opcode >> 8) & 0x0F] = chip8->delayTimer;
+				case 0x07: // Fx07 (LD Vx, DT): Set Vx = delay timer value.
+					chip8->V[chip8->instruction.X] = chip8->delayTimer;
 					break;
 				case 0x15: // Fx15 (LD DT, Vx): Set delay timer = Vx.
-					chip8->delayTimer = chip8->V[(chip8->opcode >> 8) & 0x0F];
+					chip8->delayTimer = chip8->V[chip8->instruction.X];
 					break;
 				case 0x1E: // Fx1E (ADD I, Vx): Set I = I + Vx
-					chip8->I += chip8->V[(chip8->opcode >> 8) & 0x0F];
+					chip8->I += chip8->V[chip8->instruction.X];
 					break;
-				case 0x33:
-					uint8_t value = chip8->V[(chip8->opcode >> 8) & 0x0F];
+				case 0x33: // Fx33 (LD B, Vx): 
+					uint8_t value = chip8->V[chip8->instruction.X];
 
 					chip8->memory[chip8->I] = value / 100;
 					chip8->memory[chip8->I + 1] = (value / 10) % 10;
@@ -253,7 +267,7 @@ void execute(chip8_t *chip8) {
 					break;
 				case 0x55: // Fx55 (LD [I], Vx): Store registers V0 through Vx in memory starting at location I.
 					{
-					uint8_t x = (chip8->opcode >> 8) & 0x0F;
+					uint8_t x = chip8->instruction.X;
 					for (uint8_t i = 0; i <= x; i++) {
 						chip8->memory[chip8->I + i] = chip8->V[i];
 					}
@@ -261,7 +275,7 @@ void execute(chip8_t *chip8) {
 					}
 				case 0x65: // Fx65 (LD Vx, [I]): Read registers V0 through Vx from memory starting at location I.
 					{
-					uint8_t x = (chip8->opcode >> 8) & 0x0F;
+					uint8_t x = chip8->instruction.X;
 					for (uint8_t i = 0; i <= x; i++) {
 						chip8->V[i] = chip8->memory[chip8->I + i];
 					}
@@ -414,20 +428,8 @@ int main(int argc, char **argv) {
 			SDL_RenderPresent(renderer);
 			draw = false;
 		}
-
 		SDL_Delay(3);
 	}
-
-	//int counter = 0;
-	//for (int i = 0; i < 4096; i++) {
-	//	if ((counter % 16) == 0) {
-	//		printf("\n");
-	//		counter = 0;
-	//	}
-	//	printf("%02X ", chip8.memory[i]);
-	//	counter++;
-	//}
-	//printf("\n");
 	
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
